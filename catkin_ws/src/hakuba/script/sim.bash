f0bfc09c9e90beedfdfb8aa7074a5bd6c1a299a2
ogiwara <yushiogiwara@gmail.com>
2021-03-23 12:55:52 +0900

    add DB

---
+++ catkin_ws/src/hakuba/script/sim.bash
@@ -0,0 +1,4 @@
+export SVGA_VGPU10=0;
+roslaunch yamasemi_sim intro_2020.launch;
+roslaunch beego_gazebo beego.launch;
+
--- catkin_ws/src/hakuba/src/time_sync.cpp
+++ catkin_ws/src/hakuba/src/time_sync.cpp
@@ -3,17 +3,23 @@
 #include <sensor_msgs/LaserScan.h>
 #include <message_filters/subscriber.h>
 #include <tf/message_filter.h>
+#include <map>
+#include <vector>
 
 #include "beego_controller.h"
 
 using namespace std;
 using namespace ros;
+using namespace sensor_msgs;
+using namespace geometry_msgs;
 
 struct GridMap{
-    size_t height;
-    size_t width;
+    size_t height = 0;
+    size_t width = 0;
 
-    vector<int8_t> data;
+    vector<int8_t> data{};
+
+    GridMap() = default;
 
     GridMap(size_t height_, size_t width_):
             height(height_), width(width_){
@@ -36,35 +42,14 @@ struct GridMap{
         out_msg.data = data;
     }
 
-};
-
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "time_sync"); //ノード名の初期化
-    BeegoController b;
-    ros::Duration(1.0).sleep(); // 1.0秒待機
-    ros::spinOnce(); // はじめにコールバック関数を呼んでおく
-
-
-    auto map_pub = b.nh_.advertise<nav_msgs::OccupancyGrid>("/map", 1, true);
-    auto map_meta_pub = b.nh_.advertise<nav_msgs::MapMetaData>("/map_metadata", 1, true);
-
-    while(ros::ok()){
-        sensor_msgs::LaserScan scan;
-        b.getCurrentScan(scan);
-        geometry_msgs::Pose pose;
-        b.getCurrentPose(pose);
-
-        constexpr size_t HEIGHT = 1000;
-        constexpr size_t WIDTH = 1000;
-
-        GridMap gMap(HEIGHT, WIDTH);
-
-        cout << "min: " << scan.angle_min << " max: " << scan.angle_max << endl;
+    static void fromScan(size_t height, size_t width,
+                         const Pose &pose,
+                         const LaserScan &scan, GridMap &out_map){
+        out_map = GridMap(height, width);
 
         auto current_angle = scan.angle_min;
         for(auto scan_distance : scan.ranges){
-            size_t ratio = 70;
+            size_t ratio = 100;
             assert(scan_distance > 0);
 
             float x = scan_distance * cos(current_angle);
@@ -74,22 +59,65 @@ int main(int argc, char **argv)
             x += pose.position.x;
             y += pose.position.y;
 
-            auto theta = b.calcYaw(pose);
+            tf::Quaternion quat(pose.orientation.x, pose.orientation.y,
+                                pose.orientation.z, pose.orientation.w);
+            double roll, pitch, yaw;
+            tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+            auto theta = yaw;
             auto g_x = cos(theta) * x - sin(theta) * y;
             auto g_y = sin(theta) * x + cos(theta) * y;
 
             // convert to Grid map coordinate
-            int x2 = floor(g_x * ratio) + (WIDTH / 2);
-            int y2 = floor(g_y * ratio) + (HEIGHT / 2);
+            int x2 = floor(g_x * ratio) + (width / 2);
+            int y2 = floor(g_y * ratio) + (height / 2);
 
-            if(x2 < 0 or x2 >= WIDTH or y2 < 0 or y2 >= HEIGHT ){
+            if(x2 < 0 or x2 >= width or y2 < 0 or y2 >= height ){
                 continue;
             }
 
-            gMap.set(x2, y2, 100);
+            out_map.set(x2, y2, 100);
             assert(scan.angle_increment > 0);
             current_angle += scan.angle_increment;
         }
+    }
+
+};
+
+struct DB{
+    map<ros::Time, sensor_msgs::LaserScan> lidarScan;
+
+    void greatest_less(const Time &time, LaserScan &out_scan){
+        auto it = lidarScan.lower_bound(time);
+        if(it != lidarScan.begin()){
+            out_scan = it->second;
+        }
+        out_scan = lidarScan.end()->second;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "time_sync"); //ノード名の初期化
+    BeegoController b;
+    ros::Duration(1.0).sleep(); // 1.0秒待機
+    ros::spinOnce(); // はじめにコールバック関数を呼んでおく
+
+    auto map_pub = b.nh_.advertise<nav_msgs::OccupancyGrid>("/map", 1, true);
+    auto map_meta_pub = b.nh_.advertise<nav_msgs::MapMetaData>("/map_metadata", 1, true);
+
+    size_t count = 0;
+
+    while(ros::ok() and count < 4){
+        sensor_msgs::LaserScan scan;
+        b.getCurrentScan(scan);
+        geometry_msgs::Pose pose;
+        b.getCurrentPose(pose);
+
+        constexpr size_t HEIGHT = 1000;
+        constexpr size_t WIDTH = 1000;
+
+        GridMap gMap;
+        GridMap::fromScan(HEIGHT, WIDTH, pose, scan, gMap);
 
         nav_msgs::OccupancyGrid grid;
         gMap.buildMessage(grid);
@@ -98,6 +126,8 @@ int main(int argc, char **argv)
         map_meta_pub.publish(grid.info);
 
         ros::Duration(1.0).sleep();
+
+        ++count;
     }
     return 0;
 }
