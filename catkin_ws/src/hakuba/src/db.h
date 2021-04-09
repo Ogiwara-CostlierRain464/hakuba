741a5f40a18b1accf9f74a70b12a2d30f1c6a978
ogiwara <yushiogiwara@gmail.com>
2021-04-09 13:22:00 +0900

    add sample code

--- catkin_ws/src/hakuba/src/db.h
+++ catkin_ws/src/hakuba/src/db.h
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <ros/duration.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
 #include <functional>
 #include <ecl/threads.hpp>
 #include <ecl/utilities.hpp>
@@ -14,7 +15,7 @@
 template <typename T>
 class TimeSeriesTable{
 public:
-    typedef std::map<ros::Time, T> TableType;
+    typedef std::map<ros::Time, T> TableType ;
 
     explicit TimeSeriesTable()= default;
 
@@ -37,6 +38,12 @@ public:
     void getLatest(T& out_data){
         tableMutex.lock();
         out_data = table[getLatestTime()];
+        tableMutex.unlock();
+    }
+
+    void rangeQuery(const std::function<void (TableType&)> &range_query){
+        tableMutex.lock();
+        range_query(table);
         tableMutex.unlock();
     }
 
@@ -87,5 +94,60 @@ private:
     TableType table;
     ecl::Mutex tableMutex;
 };
+
+// return result as stream
+// return big batch
+// find the data which is near at this point
+/**
+ * To implement this data, we have to implement storage storing
+ * Because memory pressure requirement is critical.
+ *
+ * - Read "DBML impl by Rust?"
+ * - How to impl memory pressure management? (because data should be saved 10s~1h back)
+ *
+ * but anyway, at first, just implement IF.
+ *
+ * after that, let's discuss about the performance.
+ *
+ * save data of odom and LiDAR
+ * find data by range query ( near at this boor ball, near with this scan data. )
+ * performance of this find query is critical!
+ *
+ *
+ */
+
+void user(){
+    TimeSeriesTable<sensor_msgs::LaserScan> scanTable;
+    TimeSeriesTable<nav_msgs::Odometry> odomTable;
+
+    sensor_msgs::LaserScan current_scan;
+    nav_msgs::Odometry current_odom;
+
+    bool scan_flag = false;
+    bool odom_flag = false;
+
+    scanTable.rangeQuery([&current_scan, &scan_flag](TimeSeriesTable<sensor_msgs::LaserScan>::TableType
+    &table){
+        for(auto & it : table){
+            if(it.second.ranges == current_scan.ranges){
+                scan_flag = true;
+            }
+        }
+    });
+    odomTable.rangeQuery([&current_odom, &odom_flag](TimeSeriesTable<nav_msgs::Odometry>::TableType
+    &table){
+        for(auto & it : table){
+            if(it.second.pose == current_odom.pose){
+                odom_flag = true;
+            }
+        }
+    });
+
+    if(scan_flag and odom_flag){
+        // do something at here
+    }
+
+
+}
 
 #endif //SRC_DB_H
